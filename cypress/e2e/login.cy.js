944459afba7d6bbe9aaf6281bc7e4b01acc84b57
ericdouglasffaria <ericdouglasffaria@hotmail.com.br>
2023-04-17 16:34:25 -0300

    Atualização - Login - Pré-Cadastro - Produto

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,22 +1,27 @@
 /// <reference types="cypress"/>
 
+beforeEach(() => {
+  cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/');
+});
+
+afterEach(() => {
+  cy.screenshot();
+});
+
 context('Funcionalidade login', () => {
   it('Deve fazer login com sucesso.', () => {
-    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/');
     cy.get('#username').type('aluno_ebac@teste.com');
     cy.get('#password').type('teste@teste.com');
     cy.get('.woocommerce-form > .button').click();
   });
 
   it('Deve emitir uma mensagem de erro, Usuario Invalido!', () => {
-    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/');
     cy.get('#username').type('aluno_ebac@teste');
     cy.get('#password').type('teste@teste.com');
     cy.get('.woocommerce-form > .button').click();
   });
 
   it('Deve emitir uma mensagem de erro, Senha Invalida!', () => {
-    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/');
     cy.get('#username').type('aluno_ebac@teste.com');
     cy.get('#password').type('teste@teste');
     cy.get('.woocommerce-form > .button').click();
